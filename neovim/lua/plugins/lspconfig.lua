83a5fde1b7363f46dcfed5c3abd401fd01c67ef5
Ben Fowler <ben.fowler@refinitiv.com>
2021-08-31 03:34:40 +0100

    feat(LSP): 'faded error virtual text' hack

--- neovim/lua/plugins/lspconfig.lua
+++ neovim/lua/plugins/lspconfig.lua
@@ -350,7 +350,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = {
             prefix = "",  -- "«"
-            spacing = 6,
+            spacing = 4,
         },
         signs = true,
         underline = true,
