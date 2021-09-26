6ae1370d5d0670c793d9f0ff4c56a87d19e8b225
Ben Fowler <ben.fowler.bjf@gmail.com>
2021-09-26 02:21:27 +0100

    neovim: nvim-tree: upstream breaking changes
    Lots of config moved to new setup() function

--- neovim/lua/plugins/nvimtree.lua
+++ neovim/lua/plugins/nvimtree.lua
@@ -4,11 +4,10 @@ if not present then
 end
 
 
---Auto-close file exploer when quiting, in case a single buffer is left
+-- Auto-close file explorer when quitting, in case a single buffer is left
 vim.cmd([[ autocmd BufEnter * if (winnr("$") == 1 && &filetype == 'nvimtree') | q | endif ]])
 
 
-local tree_cb = tree_c.nvim_tree_callback
 local g = vim.g
 
 vim.o.termguicolors = true
@@ -17,20 +16,13 @@ g.nvim_tree_side = "left"
 g.nvim_tree_width = 25
 g.nvim_tree_ignore = { ".git", ".cache" }
 g.nvim_tree_gitignore = 1
-g.nvim_tree_auto_ignore_ft = { "dashboard" } -- don't open tree on specific file types
-g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 1 -- closes tree when it's the last window
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
-g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_tab_open = 0
 g.nvim_tree_allow_resize = 1
 g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_disable_netrw = 1
-g.nvim_tree_hijack_netrw = 0
 
 g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
 g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
@@ -38,7 +30,6 @@ g.nvim_tree_lsp_diagnostics = 1 -- 0 by default, will show LSP diagnostics in th
 g.nvim_tree_disable_window_picker = 1
 
 g.nvim_tree_icon_padding = "  "
-g.nvim_tree_update_cwd = 0
 g.nvim_tree_respect_buf_cwd = 1
 
 -- List of filenames that gets highlighted with NvimTreeSpecialFile
@@ -88,3 +79,26 @@ g.nvim_tree_icons = {
         -- symlink_open = "",
     },
 }
+
+
+-- Breaking change: authors moved a bunch of config into new setup() function
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  auto_close          = true,
+  open_on_tab         = false,
+  hijack_cursor       = false,
+  update_cwd          = false,
+  update_focused_file = {
+    enable      = true,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+}
+
