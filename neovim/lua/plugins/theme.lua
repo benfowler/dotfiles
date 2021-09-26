c2fefd4555cc89270a78938077129240f2d917be
Ben Fowler <ben.fowler.bjf@gmail.com>
2021-09-26 02:23:20 +0100

    neovim: Neovim broke LSP diag symbols (again)

--- neovim/lua/plugins/theme.lua
+++ neovim/lua/plugins/theme.lua
@@ -98,6 +98,11 @@ function my_highlights_nord()
     u.HiLink("DiagnosticLineNrInfo", "DiagnosticInfo", true)
     u.HiLink("DiagnosticLineNrHint", "DiagnosticHint", true)
 
+    u.HiLink("DiagnosticSignError", "DiagnosticError", true)
+    u.HiLink("DiagnosticSignWarn", "DiagnosticWarn", true)
+    u.HiLink("DiagnosticSignInfo", "DiagnosticInfo", true)
+    u.HiLink("DiagnosticSignHint", "DiagnosticHint", true)
+
     u.Hi("LspReferenceRead", { guifg = nord14, gui = "bold,underline" })
     u.Hi("LspReferenceWrite", { guifg = nord15, gui = "bold,underline" })
     u.Hi("LspReferenceText", { gui = "bold" })
