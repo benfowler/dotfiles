b0e17453bf60eac46e1b6c1f43e37a2b0c3316e7
Ben Fowler <ben.fowler.bjf@gmail.com>
2022-04-01 12:27:23 +0100

    enable math display in Markdown (for uni)

--- neovim/lua/plugins/others.lua
+++ neovim/lua/plugins/others.lua
@@ -116,6 +116,7 @@ M.markdown = function()
     opt.formatoptions = "jtcqlnr"
     g.vim_markdown_new_list_item_indent = 2
     g.vim_markdown_auto_insert_bullets = 0
+    g.vim_markdown_math = 1
 end
 
 --Plug 'lervag/vimtex'
