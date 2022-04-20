fdc5693b8218e6ea8a81e433cbef4c1d9dfa1524
Ben Fowler <ben.fowler@refinitiv.com>
2022-04-20 10:07:08 +0100

    neovim: clean up nvim_signature config; tweak cmp config

--- neovim/lua/plugins/cmp.lua
+++ neovim/lua/plugins/cmp.lua
@@ -9,6 +9,10 @@ local luasnip = require("luasnip")
 
 vim.opt.completeopt = "menu,menuone,noinsert"
 
+-- floating menu/doc style tweaks
+local menustyle = { winhighlight = 'CursorLine:PmenuSel' }
+
+
 -- nvim-cmp setup
 cmp.setup {
     -- completion = {
@@ -32,13 +36,15 @@ cmp.setup {
         },
     },
     window = {
-        -- completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(menustyle),
+        documentation = cmp.config.window.bordered(menustyle),
     },
     mapping = {
         ["<C-Y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<C-K>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
         ["<C-J>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<C-N>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<C-K>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        ["<C-P>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
         ["<C-D>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
         ["<C-F>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
