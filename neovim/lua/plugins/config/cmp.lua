1b35030cd0c2982a5b21755687fa69a47a34fdf5
Ben Fowler <ben.fowler.bjf@gmail.com>
2022-05-14 17:24:34 +0100

    neovim: simplification: CMP: re-disabled cmdline popups

--- neovim/lua/plugins/config/cmp.lua
+++ neovim/lua/plugins/config/cmp.lua
@@ -102,18 +102,3 @@ cmp.setup {
     preselect = cmp.PreselectMode.None,
 }
 
-cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = "buffer" },
-    },
-})
-
-cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = "path" },
-    }, {
-        { name = "cmdline" },
-    }),
-})
