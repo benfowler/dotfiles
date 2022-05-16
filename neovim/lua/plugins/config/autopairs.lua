0e640c89b9fb9e4f3d5a0aaf6e23a001ae78c0e2
Ben Fowler <ben.fowler@refinitiv.com>
2022-05-16 22:48:46 +0100

    neovim: FIX: lazy-loading and plugin load order

--- neovim/lua/plugins/config/autopairs.lua
+++ neovim/lua/plugins/config/autopairs.lua
@@ -20,5 +20,5 @@ autopairs.setup {
     highlight_grey = "Comment",
 
     -- Don't add pairs if it already has a close pair in the same line
-    enable_check_bracket_line = false,
+    enable_check_bracket_line = true,
 }
