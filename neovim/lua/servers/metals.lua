78a54566733e0e1ecc5f09c60476aead83016be2
Ben Fowler <ben.fowler@refinitiv.com>
2022-05-24 19:34:25 +0100

    Scala (metals): don't launch server twice
    * Also, set shortmess=-F to get error messages for filetype

--- neovim/lua/servers/metals.lua
+++ neovim/lua/servers/metals.lua
@@ -3,22 +3,22 @@ local M = {}
 --  See issue #17 on j-hui/figet.nvim
 --  https://github.com/j-hui/fidget.nvim/issues/17
 
-local function metals_status_handler(_, status, ctx)
-  -- https://github.com/scalameta/nvim-metals/blob/main/lua/metals/status.lua#L36-L50
-  local val = {}
-  if status.hide then
-    val = {kind = "end"}
-  elseif status.show then
-    val = {kind = "begin", message = status.text}
-  elseif status.text then
-    val = {kind = "report", message = status.text}
-  else
-    return
-  end
-  local info = {client_id = ctx.client_id}
-  local msg = {token = "metals", value = val}
-  -- call fidget progress handler
-  vim.lsp.handlers["$/progress"](nil, msg, info)
+local function metals_status_handler(arg1, status, ctx)
+    -- https://github.com/scalameta/nvim-metals/blob/main/lua/metals/status.lua#L36-L50
+    local val = {}
+    if status.hide then
+        val = { kind = "end" }
+    elseif status.show then
+        val = { kind = "begin", message = status.text, title = "status" }
+    elseif status.text then
+        val = { kind = "report", message = status.text, title = "status" }
+    else
+        return
+    end
+    local info = { client_id = ctx.client_id }
+    local msg = { token = "metals", value = val }
+    -- call fidget progress handler
+    vim.lsp.handlers["$/progress"](nil, msg, info)
 end
 
 local handlers = {}
