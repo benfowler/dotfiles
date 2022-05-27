16936cb6afeefab2f6e17d1162e0ab63b5b6e5f8
Ben Fowler <ben.fowler@refinitiv.com>
2022-05-27 01:04:42 +0100

    neovim: replaced Emmet plugin with far-superior Emmet LS

---
+++ neovim/lua/servers/emmet_ls.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.configure = function(on_attach, capabilities, debounce_msec)
+    return {
+        filetypes = { 'html', 'xml', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+        on_attach = on_attach,
+        capabilities = capabilities,
+        root_dir = vim.loop.cwd,
+        flags = { debounce_text_changes = debounce_msec },
+    }
+end
+
+return M
