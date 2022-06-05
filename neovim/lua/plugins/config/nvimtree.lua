683b9efce818791babd84381014ea3c6642bff1a
Ben Fowler <ben.fowler@refinitiv.com>
2022-06-05 20:00:17 +0100

    neovim: NvimTree: cleaned more deprecated options; cleanups

--- neovim/lua/plugins/config/nvimtree.lua
+++ neovim/lua/plugins/config/nvimtree.lua
@@ -6,112 +6,65 @@ end
 local lsp_icons = require("utils").diagnostic_icons.outline
 
 
--- Auto-close file explorer when quitting, in case a single buffer is left
-vim.cmd [[ autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif ]]
-
-
-local g = vim.g
-
-vim.o.termguicolors = true
-
-g.nvim_tree_side = "left"
-g.nvim_tree_width = 25
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_allow_resize = 1
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_refresh_wait = 500
-
-g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
-
-g.nvim_tree_icon_padding = "  "
-g.nvim_tree_respect_buf_cwd = 1
-
-vim.api.nvim_exec([[
-    augroup NvimTreeOptions
-        autocmd BufEnter NvimTree set cursorline
-        autocmd BufEnter NvimTree hi clear NvimTreeExecFile
-        autocmd BufEnter NvimTree hi clear NvimTreeImageFile
-    augroup END
-    ]], false)
-
-
--- List of filenames that gets highlighted with NvimTreeSpecialFile
-g.nvim_tree_special_files = {
-    ["Cargo.toml"] = true,
-    Makefile = true,
-    ["pom.xml"] = true,
-    ["package.json"] = true,
-    ["Dockerfile"] = true,
-}
-
-g.nvim_tree_show_icons = {
-    git = 0,
-    folders = 1,
-    files = 1,
-    -- folder_arrows = 1
-}
-
-g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-        unstaged = "",
-        staged = "",
-        unmerged = "",
-        renamed = "➜",
-        untracked = "★",
-        deleted = "",
-        ignored = "◌",
-    },
-    folder = {
-        -- Disable indent_markers option to get arrows working or if you want both
-        -- arrows and indent then just add the arrow icons in front ofthe default
-        -- and opened folders below!
-        -- arrow_open = "",
-        -- arrow_closed = "",
-        default = "",
-        open = "",
-        empty = "", -- 
-        empty_open = "",
-        -- default = "",
-        -- open = "",
-        -- empty = "", -- 
-        -- empty_open = "",
-        symlink = "",
-        symlink_open = "",
-        -- symlink_open = "",
-    },
-}
-
-
 -- Breaking change: authors moved a bunch of config into new setup() function
 require 'nvim-tree'.setup {
-    disable_netrw         = false,
-    hijack_netrw          = true,
-    open_on_setup         = false,
-    ignore_ft_on_setup    = {},
-    open_on_tab           = false,
-    hijack_cursor         = false,
-    update_cwd            = false,
-    renderer              = {
+    respect_buf_cwd = true,
+    view = {
+        width = 25,
+    },
+    renderer = {
+        add_trailing = true,
+        group_empty = true,
+        highlight_git = true,
         indent_markers = {
             enable = true,
         },
+        icons = {
+            show = {
+                git = false,
+            },
+            padding = " ",
+            glyphs = {
+                default = "",
+                symlink = "",
+                git = {
+                    unstaged = "",
+                    staged = "",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌",
+                },
+                folder = {
+                    default = "",
+                    open = "",
+                    empty = "", -- 
+                    empty_open = "",
+                    -- default = "",
+                    -- open = "",
+                    -- empty = "", -- 
+                    -- empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                    -- symlink_open = "",
+                },
+            },
+        },
+        special_files = { "Cargo.toml", "Makefile", "pom.xml", "package.json", "Dockerfile" },
     },
-    update_focused_file   = {
-        enable      = true,
-        update_cwd  = false,
+    update_focused_file = {
+        enable = true,
+        update_cwd = false,
         ignore_list = {}
     },
-    system_open           = {
-        cmd  = nil,
+    system_open = {
+        cmd = nil,
         args = {}
     },
-    diagnostics           = {
-        enable       = true,
-        icons        = {
+    diagnostics = {
+        enable = true,
+        icons = {
             error = lsp_icons.error,
             warning = lsp_icons.warn,
             info = lsp_icons.info,
