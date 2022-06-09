988111dd79c2f745c887eb12632b9099351727ca
Ben Fowler <ben.fowler@refinitiv.com>
2022-06-09 23:28:20 +0100

    neovim: FIX: nvim-cmp: <ESC> in command mode kicks me out

--- neovim/lua/plugins/config/cmp.lua
+++ neovim/lua/plugins/config/cmp.lua
@@ -62,7 +62,6 @@ cmp.setup {
         end, { "i", "s" }),
         ["<Esc>"] = cmp.mapping {
             i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
         },
         ["<CR>"] = cmp.mapping.confirm { select = true },
         ["<Tab>"] = cmp.mapping(function(fallback)
