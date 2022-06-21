1a40a50cc013fd279c338248d8e4525d932cb4db
Ben Fowler <ben.fowler.bjf@gmail.com>
2022-06-21 04:02:17 +0100

    GitSigns: configured Git diff signs to be aligned correctly

--- neovim/lua/plugins/config/gitsigns.lua
+++ neovim/lua/plugins/config/gitsigns.lua
@@ -6,10 +6,10 @@ end
 -- Keybindings
 local mappings = require("mappings").user_map.gitsigns
 
-gitsigns .setup {
-     signs = {
-        add = { text = "│", numhl = "GitSignsAddNr" },
-        change = { text = "│", numhl = "GitSignsChangeNr" },
+gitsigns.setup {
+    signs = {
+        add = { text = " ▎", numhl = "GitSignsAddNr" },
+        change = { text = " ▎", numhl = "GitSignsChangeNr" },
         delete = { text = "_", numhl = "GitSignsDeleteNr" },
         topdelete = { text = "‾", numhl = "GitSignsDeleteNr" },
         changedelete = { text = "~", numhl = "GitSignsChangeNr" },
