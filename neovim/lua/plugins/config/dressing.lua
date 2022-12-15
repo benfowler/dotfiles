44c7bf9b0dbc808de500b5bce6b32822eb328b1a
Ben Fowler <ben.fowler.bjf@gmail.com>
2022-12-15 16:14:33 +1000

    neovim: dressing.nvim: fix deprecated config

--- neovim/lua/plugins/config/dressing.lua
+++ neovim/lua/plugins/config/dressing.lua
@@ -6,16 +6,20 @@ end
 
 dressing.setup {
     input = {
-        winblend = 0,
-        winhighlight = "FloatBorder:DressingFloatBorder",
+        win_options = {
+            winblend = 0,
+            winhighlight = "FloatBorder:DressingFloatBorder",
+        }
     },
     select = {
         backend = { "telescope" },
         builtin = {
-            relative = "cursor",
-            winblend = 0,
-            winhighlight = "FloatBorder:DressingFloatBorder",
             min_height = { 3 },
+            relative = "cursor",
+            win_options = {
+                winblend = 0,
+                winhighlight = "FloatBorder:DressingFloatBorder",
+            },
         },
     },
 }
