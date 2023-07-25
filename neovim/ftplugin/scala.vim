c95731c8df801f5edd398a9ac0c92178f84528c7
Ben Fowler <ben.fowler.bjf@gmail.com>
2023-07-25 19:24:47 +0100

    neovim: scala: move autocmd to ftplugin

---
+++ neovim/ftplugin/scala.vim
@@ -0,0 +1,8 @@
+if has('nvim-0.5')
+augroup scala_ft_local
+    autocmd! * <buffer>
+    au FileType scala,sbt lua vim.opt_global.shortmess:remove("F")
+    au FileType scala,sbt nnoremap <Leader>m :lua require("telescope").extensions.metals.commands()<CR>
+augroup end
+endif
+
--- neovim/lua/config/autocmds.lua
+++ neovim/lua/config/autocmds.lua
@@ -81,14 +81,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
     augroup END
 
-    if has('nvim-0.5')
-    augroup scala_ft_local
-        autocmd! * <buffer>
-        au FileType scala,sbt lua vim.opt_global.shortmess:remove("F")
-        au FileType scala,sbt nnoremap <Leader>m :lua require("telescope").extensions.metals.commands()<CR>
-    augroup end
-    endif
-
     " 'terminal' file type
     " (NeoVim's terminal defaults are bad; fix them)
 
