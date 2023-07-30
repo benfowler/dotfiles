1c4cef9714b9b30014cd0f21aaac90b7834ae3fc
Ben Fowler <ben.fowler.bjf@gmail.com>
2023-07-30 23:19:46 +0100

    neovim: kill lspzero: base scratch LSP setup from LazyVim

--- neovim/lua/util/init.lua
+++ neovim/lua/util/init.lua
@@ -2,20 +2,6 @@
 
 local M = {}
 
--- Globals - usable from command mode
-
--- put(): shorthand for print(vim.inspect(...))
-function _G.put(...)
-    local objects = {}
-    for i = 1, select("#", ...) do
-        local v = select(i, ...)
-        table.insert(objects, vim.inspect(v))
-    end
-
-    print(table.concat(objects, "\n"))
-    return ...
-end
-
 -- Diagnostic icons
 M.diagnostic_icons = {
     filled = {
@@ -31,5 +17,33 @@ M.diagnostic_icons = {
         hint = "",
     },
 }
+
+-- put(): shorthand for print(vim.inspect(...))
+function _G.put(...)
+    local objects = {}
+    for i = 1, select("#", ...) do
+        local v = select(i, ...)
+        table.insert(objects, vim.inspect(v))
+    end
+
+    print(table.concat(objects, "\n"))
+    return ...
+end
+
+---@param on_attach fun(client, buffer)
+function M.on_attach(on_attach)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
+end
+
+---@param plugin string
+function M.has(plugin)
+  return require("lazy.core.config").plugins[plugin] ~= nil
+end
 
 return M
