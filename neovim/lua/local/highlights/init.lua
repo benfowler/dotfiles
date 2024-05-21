f8037c24e9a0ccade6bf924c66aa56bc6c9f3c6a
Ben Fowler <Ben.Fowler@sainsburys.co.uk>
2024-05-21 23:10:15 +0100

    neovim: visual refresh: tweak annoying styles

--- neovim/lua/local/highlights/init.lua
+++ neovim/lua/local/highlights/init.lua
@@ -1,8 +1,24 @@
 
+-- Customisations of stock highlights
+vim.api.nvim_set_hl(0, "mkdLink", { link = "mkdLinkDef" })
+vim.api.nvim_set_hl(0, "mkdInlineURL", { link = "mkdLinkDef" })
+
 -- Inactive statusbars: make a thin rule; align vertsplit to match.
 vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE", underline = true })
 vim.api.nvim_set_hl(0, "VertSplit", { link = "NonText" })
 vim.api.nvim_set_hl(0, "WinSeparator", { link = "NonText" })
+
+-- Plugin: Luasnip
+vim.api.nvim_set_hl(0, "LuasnipChoiceNodeVirtualText", { link = "@diff.minus" })
+vim.api.nvim_set_hl(0, "LuasnipInsertNodeVirtualText", { link = "@diff.delta" })
+
+-- Plugin: NVimtree
+vim.api.nvim_set_hl(0, "NvimTreeFolderName", { link = "NvimTreeNormal" })
+vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderIcon", { link = "NvimTreeFolderIcon" })
+vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { link = "NvimTreeNormal" })
+vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderIcon", { link = "NvimTreeFolderIcon" })
+vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { link = "NvimTreeFolderIcon" })
+vim.api.nvim_set_hl(0, "NvimTreeWinSeparator", { link = "NonText" })
 
 -- My custom winbar
 vim.api.nvim_set_hl(0, "Winbar", { link = "Comment" })
@@ -12,24 +28,6 @@ vim.api.nvim_set_hl(0, "WarnWinbarDiagIndic", { link = "DiagnosticWarn" })
 vim.api.nvim_set_hl(0, "InfoWinbarDiagIndic", { link = "DiagnosticInfo" })
 vim.api.nvim_set_hl(0, "HintWinbarDiagIndic", { link = "DiagnosticHint" })
 vim.api.nvim_set_hl(0, "OkWinbarDiagIndic", { link = "DiagnosticOk" })
-
--- VS Code-coloured kind icons for nvim-cmp
--- gray
-vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg='NONE', strikethrough=true, fg='#808080' })
--- blue
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg='NONE', fg='#569CD6' })
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link='CmpIntemAbbrMatch' })
--- light blue
-vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg='NONE', fg='#9CDCFE' })
-vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link='CmpItemKindVariable' })
-vim.api.nvim_set_hl(0, 'CmpItemKindText', { link='CmpItemKindVariable' })
--- pink
-vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg='NONE', fg='#C586C0' })
-vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link='CmpItemKindFunction' })
--- front
-vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg='NONE', fg='#D4D4D4' })
-vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link='CmpItemKindKeyword' })
-vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link='CmpItemKindKeyword' })
 
 --
 -- Configuration that needs Kitty to work
