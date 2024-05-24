e26cf7924bd05f5b37f5533dbe5389acace6ae58
Ben Fowler <ben.fowler.bjf@gmail.com>
2024-05-24 02:30:32 +0100

    neovim: fix broken LSP signature help

--- neovim/lua/plugins/lsp/init.lua
+++ neovim/lua/plugins/lsp/init.lua
@@ -364,7 +364,7 @@ return {
             -- Popups get frames with rounded corners
             -- stylua: ignore start
             vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
             -- stylua: ignore end
 
             local servers = opts.servers
