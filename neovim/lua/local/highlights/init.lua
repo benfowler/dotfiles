407b6b972e4e0a3b13f83556a7db8ce18ecae111
Ben Fowler <Ben.Fowler@sainsburys.co.uk>
2024-05-28 20:51:40 +0100

    neovim: don't try to make colors work with background=. Breaks plugins.

--- neovim/lua/local/highlights/init.lua
+++ neovim/lua/local/highlights/init.lua
@@ -1,73 +1,62 @@
+-- If using the default Neovim color scheme, force-reset all highlight groups
+if vim.g.colors_name == nil or vim.g.colors_name == "default" then
 
-local group = vim.api.nvim_create_augroup('MyCustomHighlights', { clear = true })
+    -- HACK: default colour scheme changes assigned colours depend on 'background' option.
+    --
+    -- Tweak highlight groups' properties while preserving stock colours by monkey-patching
+    -- the default colourscheme when the background colour changes
 
-vim.api.nvim_create_autocmd('OptionSet', {
-    pattern = 'background',
-    callback = function()
+    if string.find(vim.env.TERM, "xterm-kitty", 1, true) ~= nil then
 
-        -- If using the default Neovim color scheme, force-reset all highlight groups
-        if vim.g.colors_name == nil or vim.g.colors_name == "default" then
+        -- Reset highlight groups to default
+        vim.cmd [[ color default ]]
 
-            -- HACK: default colour scheme changes assigned colours depend on 'background' option.
-            --
-            -- Tweak highlight groups' properties while preserving stock colours by monkey-patching
-            -- the default colourscheme when the background colour changes
-
-            if string.find(vim.env.TERM, "xterm-kitty", 1, true) ~= nil then
-
-                -- Reset highlight groups to default
-                vim.cmd [[ color default ]]
-
-                -- Tweak stock colors for highlight groups.
-                -- (This doesn't work in Lua)
-                vim.cmd [[
-                    highlight DiagnosticUnderlineError gui=undercurl
-                    highlight DiagnosticUnderlineWarn gui=undercurl
-                    highlight DiagnosticUnderlineInfo gui=undercurl
-                    highlight DiagnosticUnderlineHint gui=undercurl
-                ]]
-            end
-        end
-
-        -- Customisations of stock highlights
-        vim.api.nvim_set_hl(0, "mkdLink", { link = "mkdLinkDef" })
-        vim.api.nvim_set_hl(0, "mkdInlineURL", { link = "mkdLinkDef" })
-        vim.api.nvim_set_hl(0, "LspCodeLens", { link = "DiagnosticOk" })
-        vim.api.nvim_set_hl(0, "LspCodeLensSeparator", { link = "NonText" })
-        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { link = "NonText" })
-        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { link = "NonText" })
-        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { link = "NonText" })
-        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { link = "NonText" })
-
-        -- Inactive statusbars: make a thin rule; align vertsplit to match.
-        vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE", underline = true })
-        vim.api.nvim_set_hl(0, "VertSplit", { link = "NonText" })
-        vim.api.nvim_set_hl(0, "WinSeparator", { link = "NonText" })
-
-        -- Plugin: Luasnip
-        vim.api.nvim_set_hl(0, "LuasnipChoiceNodeVirtualText", { link = "@diff.minus" })
-        vim.api.nvim_set_hl(0, "LuasnipInsertNodeVirtualText", { link = "@diff.delta" })
-
-        -- Plugin: NVimtree
-        vim.api.nvim_set_hl(0, "NvimTreeFolderName", { link = "NvimTreeNormal" })
-        vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderIcon", { link = "NvimTreeFolderIcon" })
-        vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { link = "NvimTreeNormal" })
-        vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderIcon", { link = "NvimTreeFolderIcon" })
-        vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { link = "NvimTreeFolderIcon" })
-        vim.api.nvim_set_hl(0, "NvimTreeWinSeparator", { link = "NonText" })
-
-        -- My custom winbar
+        -- Tweak stock colors for highlight groups.
+        -- (This doesn't work in Lua)
         vim.cmd [[
-            highlight link WinBar Comment
-            highlight Winbar gui=italic guibg=NONE
+            highlight DiagnosticUnderlineError gui=undercurl
+            highlight DiagnosticUnderlineWarn gui=undercurl
+            highlight DiagnosticUnderlineInfo gui=undercurl
+            highlight DiagnosticUnderlineHint gui=undercurl
         ]]
+    end
+end
 
-        vim.api.nvim_set_hl(0, "ErrorWinbarDiagIndic", { link = "DiagnosticError" })
-        vim.api.nvim_set_hl(0, "WarnWinbarDiagIndic", { link = "DiagnosticWarn" })
-        vim.api.nvim_set_hl(0, "InfoWinbarDiagIndic", { link = "DiagnosticInfo" })
-        vim.api.nvim_set_hl(0, "HintWinbarDiagIndic", { link = "DiagnosticHint" })
-        vim.api.nvim_set_hl(0, "OkWinbarDiagIndic", { link = "DiagnosticOk" })
+-- Customisations of stock highlights
+vim.api.nvim_set_hl(0, "mkdLink", { link = "mkdLinkDef" })
+vim.api.nvim_set_hl(0, "mkdInlineURL", { link = "mkdLinkDef" })
+vim.api.nvim_set_hl(0, "LspCodeLens", { link = "DiagnosticOk" })
+vim.api.nvim_set_hl(0, "LspCodeLensSeparator", { link = "NonText" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { link = "NonText" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { link = "NonText" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { link = "NonText" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { link = "NonText" })
 
-    end,
-    group = group,
-})
+-- Inactive statusbars: make a thin rule; align vertsplit to match.
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE", underline = true })
+vim.api.nvim_set_hl(0, "VertSplit", { link = "NonText" })
+vim.api.nvim_set_hl(0, "WinSeparator", { link = "NonText" })
+
+-- Plugin: Luasnip
+vim.api.nvim_set_hl(0, "LuasnipChoiceNodeVirtualText", { link = "@diff.minus" })
+vim.api.nvim_set_hl(0, "LuasnipInsertNodeVirtualText", { link = "@diff.delta" })
+
+-- Plugin: NVimtree
+vim.api.nvim_set_hl(0, "NvimTreeFolderName", { link = "NvimTreeNormal" })
+vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderIcon", { link = "NvimTreeFolderIcon" })
+vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { link = "NvimTreeNormal" })
+vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderIcon", { link = "NvimTreeFolderIcon" })
+vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { link = "NvimTreeFolderIcon" })
+vim.api.nvim_set_hl(0, "NvimTreeWinSeparator", { link = "NonText" })
+
+-- My custom winbar
+vim.cmd [[
+    highlight link WinBar Comment
+    highlight Winbar gui=italic guibg=NONE
+]]
+
+vim.api.nvim_set_hl(0, "ErrorWinbarDiagIndic", { link = "DiagnosticError" })
+vim.api.nvim_set_hl(0, "WarnWinbarDiagIndic", { link = "DiagnosticWarn" })
+vim.api.nvim_set_hl(0, "InfoWinbarDiagIndic", { link = "DiagnosticInfo" })
+vim.api.nvim_set_hl(0, "HintWinbarDiagIndic", { link = "DiagnosticHint" })
+vim.api.nvim_set_hl(0, "OkWinbarDiagIndic", { link = "DiagnosticOk" })
