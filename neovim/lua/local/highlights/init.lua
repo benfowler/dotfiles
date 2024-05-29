636cf7ad26b836a7ef34bded0c56823bf8d50d37
Ben Fowler <ben.fowler.bjf@gmail.com>
2024-05-29 01:47:55 +0100

    neovim: LSP: diagnostic highlight tweaks

--- neovim/lua/local/highlights/init.lua
+++ neovim/lua/local/highlights/init.lua
@@ -31,6 +31,10 @@ vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { link = "NonText" })
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { link = "NonText" })
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { link = "NonText" })
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { link = "NonText" })
+vim.api.nvim_set_hl(0, "DiagnosticLineNrError", { link = "DiagnosticError" })
+vim.api.nvim_set_hl(0, "DiagnosticLineNrWarn", { link = "DiagnosticWarn" })
+vim.api.nvim_set_hl(0, "DiagnosticLineNrInfo", { link = "DiagnosticInfo" })
+vim.api.nvim_set_hl(0, "DiagnosticLineNrHint", { link = "DiagnosticHint" })
 
 -- Inactive statusbars: make a thin rule; align vertsplit to match.
 vim.cmd [[ highlight StatusLineNC guifg=NONE guisp=NvimDarkGrey4 guibg=NONE gui=underline ]]
