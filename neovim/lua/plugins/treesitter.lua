6924ea0bed20064d12dc1ad8c797e815e85c67dd
Ben Fowler <Ben.Fowler@sainsburys.co.uk>
2025-09-17 11:12:45 +0100

    neovim: fix treesitter and XML autotags

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,11 +1,15 @@
 return {
+
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function ()
-            local configs = require("nvim-treesitter.configs")
+        dependencies = {
+            { "windwp/nvim-ts-autotag" },
+        },
+        config = function()
+            local configs = require "nvim-treesitter.configs"
 
-            configs.setup({
+            configs.setup {
                 ensure_installed = {
                     "bash",
                     "html",
@@ -25,10 +29,13 @@ return {
                     "yaml",
                     -- or "all" (not recommended)
                 },
+                autotag = {
+                    enable = true,
+                },
                 sync_install = false,
                 highlight = {
                     enable = true,
-                    disable = { "markdown" }
+                    disable = { "markdown" },
                 },
                 indent = {
                     enable = true,
@@ -38,8 +45,18 @@ return {
                 },
                 matchup = {
                     enable = true,
-                }
-            })
-        end
-    }
+                },
+            }
+        end,
+    },
+
+    {
+        -- XML autocomplete
+        "windwp/nvim-ts-autotag",
+        lazy = false,
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+    },
+
 }
