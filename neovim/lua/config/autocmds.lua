a4df3ce819150d7d9475a46b7883a5a6860a7657
Ben Fowler <Ben.Fowler@sainsburys.co.uk>
2025-09-25 23:11:58 +0100

    neovim: nvim 0.11+ config: autocmd tidyups

--- neovim/lua/config/autocmds.lua
+++ neovim/lua/config/autocmds.lua
@@ -1,6 +1,20 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("my_nvim_" .. name, { clear = true })
+end
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup "checktime",
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd "checktime"
+    end
+  end,
+})
+
 -- Create directories as required when saving files
 vim.api.nvim_create_autocmd("BufWritePre", {
-    group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+    group = augroup "auto_create_dir",
     callback = function(event)
         local file = vim.loop.fs_realpath(event.match) or event.match
 
@@ -23,6 +37,7 @@ vim.api.nvim_create_user_command("YankLocation", yank_location_fn, {})
 
 -- Restore cursor position
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    group = augroup "last_loc",
     pattern = { "*" },
     callback = function()
         vim.api.nvim_exec('silent! normal! g`"zv', false)
@@ -36,16 +51,16 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 })
 
 -- Highlight on yank
-local yankGrp = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
+    group = augroup "yank_highlight",
     callback = function()
         vim.highlight.on_yank { timeout = 700 }
     end,
-    group = yankGrp,
 })
 
 -- Show cursor line only in active window
 vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+    group = augroup "show_cl_only_in_active_win_on",
     callback = function()
         local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
         if ok and cl then
@@ -56,6 +71,7 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
 })
 
 vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+    group = augroup "show_cl_only_in_active_win_off",
     callback = function()
         local cl = vim.wo.cursorline
         if cl then
@@ -67,6 +83,7 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
 
 -- Hide statusline for various filetypes
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter", "CmdwinEnter", "TermEnter" }, {
+    group = augroup "hide_statusline_for_win",
     callback = function()
         if vim.bo.ft == "NvimTree" then
             vim.go.laststatus = 0
@@ -79,28 +96,42 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter", "CmdwinEnte
 -- Terminal customisations.
 
 vim.api.nvim_create_autocmd("TermOpen", {
+    group = augroup "term_open_opts",
     pattern = "term://*",
     command = "setlocal listchars= nonumber norelativenumber nocursorline nospell",
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
+    group = augroup "term_open_ft",
     pattern = "term://*",
     command = "setfiletype terminal",
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
+    group = augroup "term_open_insert",
     pattern = "term://*",
     command = "startinsert",
 })
 
 -- Close terminal buffer on prcess exit
 vim.api.nvim_create_autocmd("TermClose", {
+    group = augroup "term_close",
     pattern = vim.fn.expand "$SHELL" .. "\\|zsh",
     command = ":bd",
 })
 
+-- Set filetype for .env and .env.* files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup "env_filetype",
+  pattern = { "*.env", ".env.*" },
+  callback = function()
+    vim.opt_local.filetype = "sh"
+  end,
+})
+
 -- Detect AWS CloudFormation templates
 vim.api.nvim_create_autocmd({ "BufRead" }, {
+    group = augroup "detect_cfn",
     pattern = { "*.yaml" },
     callback = function()
         local header = vim.api.nvim_buf_get_lines(0, 0, 2, false)
@@ -110,3 +141,4 @@ vim.api.nvim_create_autocmd({ "BufRead" }, {
         end
     end,
 })
+
