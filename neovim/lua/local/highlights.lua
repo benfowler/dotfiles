a20a03737e5dadd85052b3efcc430a9785563643
Ben Fowler <Ben.Fowler@sainsburys.co.uk>
2025-10-09 20:17:20 +0100

    neovim: nvim 0.11+ config: Lua: tweak highlights

--- neovim/lua/local/highlights.lua
+++ neovim/lua/local/highlights.lua
@@ -39,32 +39,56 @@ if vim.g.colors_name == nil or vim.g.colors_name == "default" then
     vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none"})
     vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none"})
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 end
 
 -- Customisations of stock highlights
 vim.api.nvim_set_hl(0, "ColorColumn", { link = "Pmenu" })
 vim.api.nvim_set_hl(0, "FoldColumn", { link = "NonText" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { link = "NonText" })
+vim.api.nvim_set_hl(0, "QuickFixLine", { link = "Pmenu" })
+vim.api.nvim_set_hl(0, "NormalFloat", { link = "Pmenu" })
 
-vim.api.nvim_set_hl(0, "mkdLinkDef", { fg="NvimLightBlue", underline=true })
+
+---
+--- See <https://gist.github.com/swarn/fb37d9eefe1bc616c2a7e476c0bc0316>
+--- for a comprehensive guide to Semantic highlighting in Neovim.
+---
+
+-- (Vim highlights)
+vim.api.nvim_set_hl(0, "Constant", { fg="NvimLightCyan" })
+
+-- (Treesitter highlights)
+vim.api.nvim_set_hl(0, "@property", { link="Normal" })
+vim.api.nvim_set_hl(0, "@constant", { fg="NvimLightCyan", italic=true })
+vim.api.nvim_set_hl(0, "@variable.parameter", { fg="NvimLightMagenta" })
+
+-- (Semantic highlights)
+vim.api.nvim_set_hl(0, "@lsp.mod.global", { fg="NvimLightRed" })
+vim.api.nvim_set_hl(0, "@lsp.type.parameter", { fg="NvimLightMagenta" })
+vim.api.nvim_set_hl(0, "@lsp.type.property", { link="Normal" })
+vim.api.nvim_set_hl(0, "@lsp.type.variable", { fg="NvimLightBlue" })
+
+vim.api.nvim_set_hl(0, "mkdLinkDef", { fg="NvimLightBlue" })
 vim.api.nvim_set_hl(0, "mkdLink", { link = "mkdLinkDef" })
 vim.api.nvim_set_hl(0, "mkdInlineURL", { link = "mkdLinkDef" })
 vim.api.nvim_set_hl(0, "@markup.link.label.markdown_inline", { link = "mkdLinkDef" })
 vim.api.nvim_set_hl(0, "@markup.link.url.markdown_inline", { link = "mkdLinkDef" })
 vim.api.nvim_set_hl(0, "@markup.quote.markdown", { link = "mkdBlockQuote" })
 vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { link = "mkdCode" })
-vim.api.nvim_set_hl(0, "Identifier", { link="NonText" })
 
-vim.api.nvim_set_hl(0, "LspCodeLens", { fg="NvimLightMagenta" })
+vim.api.nvim_set_hl(0, "LspCodeLens", { link = "NonText" })
 vim.api.nvim_set_hl(0, "LspCodeLensSeparator", { link = "Comment" })
 vim.api.nvim_set_hl(0, "LspReferenceRead", { fg="Black", bg= "NvimLightGreen" })
+vim.api.nvim_set_hl(0, "LspReferenceWrite", { fg="Black", bg= "NvimLightRed" })
 
 vim.api.nvim_set_hl(0, "DiagnosticLineNrError", { link = "DiagnosticError" })
 vim.api.nvim_set_hl(0, "DiagnosticLineNrWarn", { link = "DiagnosticWarn" })
 vim.api.nvim_set_hl(0, "DiagnosticLineNrInfo", { link = "DiagnosticInfo" })
 vim.api.nvim_set_hl(0, "DiagnosticLineNrHint", { link = "DiagnosticHint" })
 vim.cmd [[ highlight LspInlayHint guifg=NvimDarkGrey4 guibg=NONE gui=italic ]]
+
+-- Active statusbar override for default theme
+vim.cmd [[ highlight StatusLine cterm=reverse guifg=#a0a8b7 guibg=#30363f ]]
 
 -- Inactive statusbars: make a thin rule; align vertsplit to match.
 vim.cmd [[ highlight StatusLineNC guifg=NONE guisp=NvimDarkGrey4 guibg=NONE gui=underline ]]
@@ -99,12 +123,22 @@ vim.api.nvim_set_hl(0, "HintWinbarDiagIndic", { link = "DiagnosticHint" })
 vim.api.nvim_set_hl(0, "OkWinbarDiagIndic", { link = "DiagnosticOk" })
 
 -- My custom statusbar  (NOTE: linked highlights chosen to look nicer, not match logicically)
+
+vim.cmd [[
+    highlight MiniStatusLineModeInsert gui=bold guifg=#1f2329 guibg=#4fa6ed
+    highlight MiniStatusLineModeCommand gui=bold guifg=#1f2329 guibg=#e2b86b
+    highlight MiniStatusLineModeOther gui=bold guifg=#1f2329 guibg=#48b0bd
+    highlight MiniStatusLineModeReplace gui=bold guifg=#1f2329 guibg=#e55561
+    highlight MiniStatusLineModeNormal gui=bold guifg=#1f2329 guibg=#8ebd6b
+    highlight MiniStatusLineModeVisual gui=bold guifg=#1f2329 guibg=#bf68d9
+]]
+
 vim.api.nvim_set_hl(0, "StatusLineModeNormal", { bold = true })
-vim.api.nvim_set_hl(0, "StatusLineModeCommand", { bg="NvimLightBlue", bold = true })
-vim.api.nvim_set_hl(0, "StatusLineModeEx", { bg="NvimLightBlue", bold = true })
-vim.api.nvim_set_hl(0, "StatusLineModeInsert", { bg="NvimLightCyan", bold = true })
-vim.api.nvim_set_hl(0, "StatusLineModeReplace", { bg="NvimLightRed", bold = true })
-vim.api.nvim_set_hl(0, "StatusLineModeTerminal", { bg="NvimLightGreen", bold = true })
-vim.api.nvim_set_hl(0, "StatusLineModeVisual", { bg="NvimLightYellow", bold = true })
-vim.api.nvim_set_hl(0, "StatusLineModeSelect", { bg="NvimLightMagenta", bold = true })
+vim.api.nvim_set_hl(0, "StatusLineModeCommand", { link = "MiniStatusLineModeInsert" })
+vim.api.nvim_set_hl(0, "StatusLineModeEx", { link = "MiniStatusLineModeCommand" })
+vim.api.nvim_set_hl(0, "StatusLineModeInsert", { link = "MiniStatusLineModeOther" })
+vim.api.nvim_set_hl(0, "StatusLineModeReplace", { link = "MiniStatusLineModeReplace" })
+vim.api.nvim_set_hl(0, "StatusLineModeTerminal", { link = "MiniStatusLineModeNormal" })
+vim.api.nvim_set_hl(0, "StatusLineModeVisual", { link = "MiniStatusLineModeCommand" })
+vim.api.nvim_set_hl(0, "StatusLineModeSelect", { link = "MiniStatusLineModeVisual" })
 
