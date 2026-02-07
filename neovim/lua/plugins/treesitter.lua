3538a1fc5176f469fc504d0cb21c8307b41eec8a
Ben Fowler <Ben.Fowler@sainsburys.co.uk>
2026-02-07 03:19:41 +0000

    neovim: fix Treesitter breakage

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -8,41 +8,23 @@ return {
             { "windwp/nvim-ts-autotag" },
         },
         config = function()
-            require("nvim-treesitter.configs").setup {
-
-                ensure_installed = {
-                    "bash",
-                    "html",
-                    "java",
-                    "javascript",
-                    "json",
-                    "kotlin",
-                    "lua",
-                    "python",
-                    "scala",
-                    "toml",
-                    "tsx",
-                    "typescript",
-                    "vue",
-                    "xml",
-                    "yaml",
-                    -- or "all" (not recommended)
-                },
-                sync_install = false,
-                highlight = {
-                    enable = true,
-                    disable = { "markdown" },
-                },
-                indent = {
-                    enable = true,
-                    disable = {
-                        "markdown", -- indentation at bullet points is worse
-                    },
-                },
-                matchup = {
-                    enable = true,
-                },
-            }
+            require('nvim-treesitter').install({
+                "bash",
+                "html",
+                "java",
+                "javascript",
+                "json",
+                "kotlin",
+                "lua",
+                "python",
+                "scala",
+                "toml",
+                "tsx",
+                "typescript",
+                "vue",
+                "xml",
+                "yaml"
+             })
         end,
     },
 
